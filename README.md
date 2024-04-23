3618bede5c5ce3bbd8a79412131daf19c61e9aa9
Mendes <daniel.mendes01.ti@gmail.com>
2024-04-23 18:09:59 -0300

    v1.0 com bugs a resolver

---
+++ README.md
@@ -0,0 +1,24 @@
+# Desafio de Projeto: Sistema Bancário em Python
+
+Este repositório contém a implementação de um sistema bancário em Python, desenvolvido como parte do desafio da Digital Innovation One (DIO).
+
+## Descrição
+
+O desafio consiste em criar um sistema bancário em Python, com funcionalidades básicas como cadastro de clientes, realização de saques, depósitos e transferências.
+
+| Versão       | Link                                                                                                                                                          |
+|--------------|---------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| Versão Antiga| [Aula referência para desenvolver a primeira versão do código](https://web.dio.me/project/desafio-de-projeto-criando-um-sistema-bancario/learning/fa812356-0da6-4a85-9ffb-8b255748a288?back=/track/formacao-python-developer&tab=undefined&moduleId=undefined)   |
+| Versão Otimizada | [Aula referência para desenvolver a versão otimizada do código](https://web.dio.me/project/otimizando-o-sistema-bancario-com-funcoes-python/learning/82a55799-cfb8-479d-85a3-4982e29c90ba?back=/track/formacao-python-developer&tab=undefined&moduleId=undefined) |
+
+## Como Executar
+
+Para executar o sistema em sua máquina local, siga estas etapas:
+
+1. Clone este repositório.
+2. Abra o terminal e navegue até o diretório do projeto.
+3. Execute o arquivo principal do sistema, por exemplo:
+
+```bash
+Obrigado pela atenção.
+Att, Mendes.
--- desafioBancoOtimizadoDIO.py
+++ desafioBancoOtimizadoDIO.py
@@ -11,8 +11,75 @@
 #  conta corrente: agencia, número da conta e usuario. numero da conta é sequencial, iniciando em 1| numero da agencia é fixo "0001". o Usuário pode ter mais de uma conta, mas uma onta pertence somente a um usuário
 
 
+print("Bem vindo(a) ao Banco MTI, essas são as nossas opções de operação")
+menu =( '''
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+             
+Qual operação deseja realizar: ''')
+
+saldo = 0
+limite= 500
+extrato= 0
+extratoDepositoList=[]
+extratoSaquesList=[]
+def function_saque(saldo, extrato):
+
+    saquesFeitos=0
+    numeroMaximoDeSaques=3
+    if saquesFeitos<numeroMaximoDeSaques:
+            while True:
+                saque=float(input("Insira aqui o valor que você deseja sacar: "))
+                if saque<0 or saque>500:
+                    print("Você tem que inserir um valor maior que R$0 e até R$500 ")
+                elif saque>extrato:
+                    print("Você não tem saldo suficiente")
+                else:
+                    extrato-=saque
+                    saquesFeitos+=1
+                    extratoSaquesList.append(saque)
+                    break
+    else: 
+          print("Você atingiu o número máximo de saques por dia: {numeroMaximoDeSaques}")
+    return saldo,extrato,saque
+
+def function_deposito(saldo, extrato):
+      deposito=float(input("Insira aqui o valor que você deseja depositar: "))
+      if deposito<0:
+        print("Você precisa inserir um valor minimo de R$1")
+      else:
+        extrato+=deposito
+        extratoDepositoList.append(deposito)
+        saldo+= deposito
+        
+        return saldo,extrato,deposito
+      
+def function_extrato(extrato,saldo):
+    for i in extratoDepositoList:
+        print(f"Segue anexo os seus depósitos: R${i}")
+    for x in extratoSaquesList:
+        print(f"Segue anexo os seus saques: R$: {x}")
+    print(f"O seu saldo atual da conta é: R${extrato:.2f}")
+    return saldo, extrato
 
 
 
 
 
+while True:
+    opção=input(menu)
+    if opção.lower()=="s":
+        saqueFun=function_saque(saldo=saldo, extrato=extrato)
+        print(saqueFun)
+    elif opção.lower()=="d":
+        depositoFun=function_deposito(saldo,extrato)
+        print(depositoFun)
+    elif opção.lower()=="e":
+        extratoFun=function_extrato(extrato,saldo)
+        print(extratoFun)
+    elif opção.lower()=="q":
+        break
+    else: 
+        print("Você digitou uma opção inválida")
